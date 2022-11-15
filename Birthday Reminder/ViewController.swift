35c81084448a19c848b26e59678f20c329bc6e83
Ivan <saitez377@gmail.com>
2022-11-15 18:23:18 +0300

    - added main screen - implemented some logic

--- Birthday Reminder/ViewController.swift
+++ Birthday Reminder/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var birthlabel: UILabel!
+    @IBOutlet var inButton: UIButton!
+    @IBOutlet var idSwitch: UISwitch!
+    @IBOutlet var faceIdLabel: UILabel!
+    @IBOutlet var emailLabel: UITextField!
+    @IBOutlet var passLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // label
+        birthlabel.layer.borderColor = UIColor.gray.cgColor
+        birthlabel.layer.borderWidth = 2
+        
+        passLabel.isSecureTextEntry = true
+        
+        // button
+        inButton.layer.cornerRadius = 10
+        
+        // switch
+        idSwitch.isOn = false
+        idSwitch.addTarget(self, action: #selector(checkIsOn(target:)), for: .valueChanged)
     }
 
-
+    // Actions
+    @IBAction func eyeButton(_ sender: Any) {
+        if passLabel.isSecureTextEntry {
+            passLabel.isSecureTextEntry = false
+        } else {
+            passLabel.isSecureTextEntry = true
+        }
+    }
+    
+    
+    @IBAction func signInAction(_ sender: Any) {
+        
+    }
+    
+    
+    // function
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        self.present(alertController, animated: true)
+    }
+    
+    
+    @objc func checkIsOn(target: UISwitch) {
+        if target.isOn {
+            faceIdLabel.textColor = UIColor.blue
+        } else {
+            faceIdLabel.textColor = UIColor.black
+        }
+    }
 }
 
---
+++ Birthday Reminder/ViewControllerTwo.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTwo.swift
+//  Birthday Reminder
+//
+//  Created by Mac on 15.11.2022.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
